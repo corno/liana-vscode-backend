319a61485156e0c9f0dac66252086d59cd66bb39
Corno Schraverus <corno@schraverus.com>
2026-02-09 23:17:43 +0100

    pdt(broken): ...

--- pub/src/implementation/manual/schemas/temp/transformers/get_completion_items.ts
+++ pub/src/implementation/manual/schemas/temp/transformers/get_completion_items.ts
@@ -1,12 +1,14 @@
 import * as _p from 'pareto-core/dist/assign'
 import * as _pi from 'pareto-core/dist/interface'
 import * as _pdev from 'pareto-core-dev'
+import _p_unreachable_code_path from 'pareto-core/dist/_p_unreachable_code_path'
 
+//data types
 import * as d_schema from "pareto-liana/dist/interface/generated/liana/schemas/schema/data/resolved"
 import * as d_in from "pareto-liana/dist/interface/to_be_generated/temp_unmashall_result"
 import * as d_token from "astn-core/dist/interface/generated/liana/schemas/token/data"
 import * as d_ast_target from "astn/dist/interface/generated/liana/schemas/authoring_target/data"
-import * as d_fpblock from "pareto-fountain-pen/dist/interface/generated/liana/schemas/block/data"
+import * as d_fpblock from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
 import * as d_out from "../../../../../interface/generated/liana/schemas/server/data"
 
 //dependencies
